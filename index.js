f1750e952ba68aa40b961122f242fc38918500dc
ahkamul <sasraishougat@gmail.com>
2021-09-12 16:22:10 +0600

    Codes on JS Functions(w3school/jsFunctionDefinitions to jsFunctionApply) added

--- index.js
+++ index.js
@@ -65,25 +65,221 @@
 
 // Stack Learner YT Video # 58 : Object Method in JS : 
 
-var obj = {
-    x : 40,
-    y : 60,
-    z : 75
-}
-// console.log(Object.keys(obj))
-// console.log(Object.values(obj))
-// console.log(Object.entries(obj))
+// var obj = {
+//     x : 40,
+//     y : 60,
+//     z : 75
+// }
+// // console.log(Object.keys(obj))
+// // console.log(Object.values(obj))
+// // console.log(Object.entries(obj))
 
-// var obj2 = obj 
-// obj2.x = 70
-// obj2.y = 100
+// // var obj2 = obj 
+// // obj2.x = 70
+// // obj2.y = 100
+
+// // console.log(obj)
+// // console.log(obj2)
+
+// var obj2 = Object.assign({}, obj)
+// obj2.x = 100
 
 // console.log(obj)
 // console.log(obj2)
 
-var obj2 = Object.assign({}, obj)
-obj2.x = 100
+// w3school / JS Function Definitions :
 
-console.log(obj)
-console.log(obj2)
+// Function Declaration :
 
+// var x = myFunction (4,3);
+
+// function myFunction (a, b){
+//     return a * b ;
+// }
+
+// console.log(x);
+
+// Function Expressions :
+
+// const x = function (a, b){
+//     return a * b
+// }
+// console.log(x)
+// console.log(x(3, 4))
+// console.log(3 , 4)  // not returning result as function
+
+ 
+// The Function () Constructor :
+
+// const myFunction = new Function ("a", "b", "return a * b")
+
+// console.log(myFunction(4 , 3));
+
+// Same result will be seen for the following code :
+
+// const myFunction = function (a , b){return a * b};
+
+// console.log(myFunction(4 , 3))
+
+// Function Hoisting :
+
+// function myFunction (a , b){
+//     return a * b;
+// }
+// let x = myFunction (4 , 3)
+
+// console.log(x)
+
+// function myFunction (a , b){
+//     return a * b;
+// }
+// let x = myFunction (4 , 3) * 2;
+
+// console.log(x);
+
+
+// w3school / JS Function Parameters :
+
+// function myFunction (x , y){
+//     if (y = undefined){
+//         y = 2;
+//     }
+//     return x * y;
+// }
+// console.log(myFunction(4));  // Getting result NaN
+
+// ECMAScript 2015 allows default parameter values in the function declaration:
+
+// function myFunction (x , y = 2){
+//     return x * y;
+// }
+// console.log(myFunction(4));
+
+// The arguments Object :
+
+// Ex. 1
+
+// function findMax () {
+//     let max = Infinity;
+//     for (let i = 0; i < arguments.length; i ++ ) {
+//         if (arguments [i] > max){
+//             max = arguments [i];
+//         }
+//     }
+//     return max;
+// }
+// console.log(findMax(4, 5, 6));  // Getting result Infinity 
+
+// Ex. 2
+
+// function sumAll (){
+//     let sum = 0;
+//     for (let i = 0; i < arguments.length; i ++){
+//         sum += arguments [i];
+//     }
+//     return sum;
+// }
+
+// console.log(sumAll(1, 2, 3, 4, 5));
+
+
+// JS Function Invocation :
+
+// Invoking (Calling) a Function as a Method :
+
+// const myObject = {
+//     firstName : "John",
+//     lastName : "Doe",
+//     fullName : function () {
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+// console.log(myObject.fullName());
+
+// const myObject = {
+//     firstName : "John", 
+//     lastName : "Doe",
+//     fullName : function (){
+//         return this;
+//     }
+// }
+// console.log(myObject.fullName());  //This will return [object Object] (The owner object)
+
+// function myFunction (arg1 , arg2){
+//     this.firstName = arg1;
+//     this.lastName = arg2;
+// }
+
+// const myObj = new myFunction ("John", "Doe");
+
+// console.log(myObj.firstName);
+
+
+// JS Function Call :
+
+// All Functions are Methods:
+
+// const myObject = {
+//     firstName : "John",
+//     lastName : "Doe", 
+//     fullName : function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+// console.log(myObject.fullName());
+
+// The JS call() Method:
+
+// const person = {
+//     fullName : function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
+
+// const person1 = {
+//     firstName : "John",
+//     lastName : "Doe"
+// }
+
+// const person2 = {
+//     firstName : "Mary",
+//     lastName : "Doe"
+// }
+
+// console.log(person.fullName.call(person1));
+// console.log(person.fullName.call(person2));
+
+// The call() Method with Arguments:
+
+// const person = {
+//     fullName : function(city, country){
+//         return this. firstName + " " + this. lastName + ", " + city + ", " + country;
+//     }
+// }
+
+// const person1 = {
+//     firstName : "John",
+//     lastName : "Doe"
+// }
+
+// console.log(person.fullName.call(person1, "Oslo", "Norway"));
+
+
+// JS Function Apply:
+
+// The apply() method is similar to the call() method, but there is a fundamental difference between them i.e. the call() method takes arguments separately, but the apply() method takes arguments as an array :
+
+// The apply() method with Arguments:
+
+const person = {
+    fullName : function (city, country){
+        return this.firstName + " " + this.lastName + ", " + city + ", " + country;
+    }
+}
+
+const person1 = {
+    firstName : "John",
+    lastName : "Doe"
+}
+
+console.log(person.fullName.apply(person1, ["Oslo"], ["Norway"]));
